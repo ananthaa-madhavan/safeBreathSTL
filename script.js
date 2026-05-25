ee30f01ccf7cfb5e869a9b49ec38840e9bf68586
ananthaa-madhavan <m.ananthaa09@gmail.com>
2026-05-25 16:50:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ function renderData(data) {
       radius: auraRadius,
       stroke: false,
       fillColor: color,
-      fillOpacity: 0.12,
+      fillOpacity: 0.02,
       className: "soft-aura"
     }).addTo(heatLayer);
 
